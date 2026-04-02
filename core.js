c0d90cb9d4e0d17e457860fbad2dc21a99bcab8d
montedopasto <mdopasto@gmail.com>
2026-04-02 11:03:48 +0100

    Update core.js

--- core.js
+++ core.js
@@ -453,8 +453,8 @@ const factorSexoAlim = (r.sexo === "F") ? 1.02 : 0.98;
 
 // idade
 let factorIdade = 1.0;
-if (r.data_nasc) {
-  const idadeDias = daysBetweenUTC(parseDatePT(r.data_nasc), today);
+if (r.dNasc) {
+  const idadeDias = daysBetweenUTC(parseDatePT(r.dNasc), today);
   if (idadeDias < 300) factorIdade = 1.05;
   else if (idadeDias > 600) factorIdade = 0.95;
 }
