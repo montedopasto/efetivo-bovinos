b1bb5228afc59cdeabc68e4876b50361f3cea5ed
montedopasto <mdopasto@gmail.com>
2026-04-13 14:37:21 +0100

    Update sharepoint-sync.js

--- sharepoint-sync.js
+++ sharepoint-sync.js
@@ -2,7 +2,7 @@
 // CONFIG SHAREPOINT
 // =============================
 
-const SITE_ID = "montedopastopt.sharepoint.com,30b32348-8df0-4dbe-9...5cbe7,3a90922f-7a65-44d9-ae1e-ef11c749a820";
+const SITE_ID = "montedopastopt.sharepoint.com,30b32348-8df0-4dbe-9244-45126c5cbec7,3a90922f-7a65-44d9-ae1e-ef11c749a820";
 const LIST_ANIMAIS_ID = "COLOCA_AQUI";
 const LIST_PESAGENS_ID = "COLOCA_AQUI";
 
