5c064edb51490de2d3c395ec0bdcd2bcc7e90132
montedopasto <mdopasto@gmail.com>
2026-04-14 08:37:14 +0100

    Update sharepoint-sync.js

--- sharepoint-sync.js
+++ sharepoint-sync.js
@@ -85,10 +85,11 @@ async function spGetPesagem(animalId, data, token){
   const j = await r.json();
 
   if(!j.value){
-  console.error("Erro Graph (Pesagem):", j);
-  return false;
-}
-return j.value.length > 0;
+    console.error("Erro Graph (Pesagem):", j);
+    return false;
+  }
+
+  return j.value.length > 0;
 }
 async function spCreatePesagem(data, token){
 
